d65cb4f3463d7e102b85e0da88f8d08ba60434bb
Jiwon Park <jiwonpark@utexas.edu>
2024-08-30 13:05:46 -0500

    Initial Commit

---
+++ DiaryApp/DiaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryAppApp.swift
+//  DiaryApp
+//
+//  Created by Jiwon Park on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct DiaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
